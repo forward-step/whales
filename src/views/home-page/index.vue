15a44a5cc0cd36bc00a004a1d9f027a7dfa23969
dengxiaojun <1718874198@qq.com>
2021-10-12 17:47:16 +0800

    [feat]轮播图组件

--- src/views/home-page/index.vue
+++ src/views/home-page/index.vue
@@ -1,16 +1,19 @@
 <template>
     <navigation-bar />
+    <dynamic-dynamic />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 import navigationBar from '@/views/home-page/components/navigation-bar/index.vue'
+import dynamicDynamic from '@/views/home-page/components/dynamic-carousel/index.vue'
 
 export default defineComponent({
     name: "HomePage",
     components: {
-        navigationBar
+        navigationBar,
+        dynamicDynamic
     }
 });
 </script>
