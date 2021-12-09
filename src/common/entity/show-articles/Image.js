2e7ba6df568567af27733010899a603402b1993b
YongKJ <dxj1718874198@gmail.com>
2021-12-09 20:05:45 +0800

    [update]文章瀑布流布局显示

--- src/common/entity/show-articles/Image.js
+++ src/common/entity/show-articles/Image.js
@@ -5,4 +5,8 @@ export class Image {
         return require("@/assets/resources/show-articles/img/044e9120-12d3-11ec-98b4-1168c7db05d9.png");
     }
 
+    static get ARTICLEONE() {
+        return require("@/assets/resources/show-articles/img/acf0fea0-f788-11eb-bf4f-b9fadce42762.png");
+    }
+
 }
--- src/views/home-page/components/show-articles/index.vue
+++ src/views/home-page/components/show-articles/index.vue
@@ -45,21 +45,35 @@
     <a-col :span="4">
       <a-card hoverable class="a-card-style">
         <template #cover>
-          <img
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          />
+          <div class="div-img" :style="imgStyle">
+            <div class="div-image" :style="imgStyle">
+              <div class="image-box" :style="articleImgStyle"></div>
+            </div>
+          </div>
         </template>
+
+        <a-card-meta>
+          <template #avatar>
+            <div class="div-badge">
+              <span class="span-badge">More</span>
+            </div>
+            <div class="div-month">
+              <span class="span-month">SEPTEMBER 30TH</span>
+            </div>
+            <div class="div-title">
+              <span>生活语言</span>
+            </div>
+            <div class="div-article">
+              <span>把圈子变小，把语言变干净，把成绩往上提，把故事往心里收一收，现在想要的以后你都会有。</span>
+            </div>
+          </template>
+        </a-card-meta>
+
         <template class="ant-card-actions" #actions>
           <setting-outlined key="setting" />
           <edit-outlined key="edit" />
           <ellipsis-outlined key="ellipsis" />
         </template>
-        <a-card-meta title="Card title" description="This is the description">
-          <template #avatar>
-            <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-          </template>
-        </a-card-meta>
       </a-card>
 
       <a-card hoverable class="a-card-style">
@@ -194,6 +208,7 @@
 
 <script>
   import { defineComponent, ref } from 'vue';
+  import {ImgUtil} from "@/common/util/ImgUtil";
   import {Image} from "@/common/entity/show-articles/Image";
   import { ReloadOutlined, SettingOutlined, EditOutlined, EllipsisOutlined } from '@ant-design/icons-vue';
 
@@ -201,9 +216,13 @@
     name: "ShowArticles",
     setup() {
       const hotIssueImg = Image.HOTISSUE;
+      const articleImgStyle = ImgUtil.getBackImgUrl(Image.ARTICLEONE);
+      const imgStyle = ImgUtil.getImgHeight(Image.ARTICLEONE);
 
       return {
-        hotIssueImg
+        hotIssueImg,
+        articleImgStyle,
+        imgStyle
       }
     },
     components: {
@@ -218,6 +237,118 @@
 <style scoped>
 
 @import "../../../../assets/resources/show-articles/css/style.css";
+
+.div-article {
+  font-size: 28px;
+  font-family: arial;
+  text-align: left;
+  color: rgb(51, 51, 51);
+  white-space: pre-wrap;
+  letter-spacing: 0px;
+  word-break: break-word;
+  line-height: 56px;
+  transform-origin: left top;
+  transform: scale(0.5);
+  transition: unset;
+  /*position: absolute;*/
+  /*top: 0px;*/
+  /*left: 0px;*/
+  /*width: 100%;*/
+}
+
+.div-title {
+  font-size: 40px;
+  font-family: "Ek Mukta", sans-serif;
+  text-align: left;
+  color: rgb(7, 7, 7);
+  font-weight: bold;
+  white-space: nowrap;
+  letter-spacing: 0px;
+  word-break: normal;
+  line-height: 56px;
+  transform-origin: left top;
+  transform: scale(0.5);
+  transition: unset;
+  margin-top: 14px;
+  margin-bottom: 14px;
+}
+
+.span-month {
+  color: rgb(153, 153, 153);
+  font-family: "Ek Mukta", sans-serif;
+  line-height: 56px;
+  font-size: 26px;
+}
+
+.div-month {
+  font-size: 26px;
+  font-family: "Ek Mukta", sans-serif;
+  text-align: left;
+  color: rgb(153, 153, 153);
+  white-space: nowrap;
+  letter-spacing: 0px;
+  word-break: normal;
+  line-height: 56px;
+  transform-origin: left top;
+  transform: scale(0.5);
+  transition: unset;
+  position: absolute;
+  top: 12px;
+  left: 62px;
+}
+
+.span-badge {
+  font-size: 16px;
+  font-family: "Arial Black";
+  text-align: center;
+  color: rgb(255, 254, 254);
+  white-space: pre-wrap;
+  letter-spacing: 0px;
+  word-break: break-word;
+  line-height: 40px;
+  padding: 0px;
+  max-width: 116px;
+  transition: unset;
+  transform: scale(0.5) translateX(-50%) translateY(-100%);
+  left: 50%;
+  width: 116px;
+  transform-origin: left center;
+  opacity: 1;
+}
+
+.div-badge {
+  width: 58px;
+  background: rgb(238, 70, 70);
+  border-radius: 5.5px;
+  transition: unset;
+}
+
+.image-box {
+  transition: unset;
+  inset: 0px;
+  background-size: cover;
+  background-position: 50%;
+  position: absolute;
+  background-repeat: no-repeat;
+}
+
+.div-image {
+  border-radius: 10.5px 10.5px 0px 0px;
+  overflow: hidden;
+  opacity: 1;
+  /*width: 218px;*/
+  height: 170px;
+  /*filter: drop-shadow(rgb(148, 146, 146) 0px 3px 6px);*/
+  transition: unset;
+}
+
+.div-img {
+  transform: translate(0px, 0px) rotate(0deg);
+  /*width: 218px;*/
+  height: 170px;
+  transition: unset;
+  z-index: 0;
+}
 
 .a-card-style1 {
   width: 218px;
