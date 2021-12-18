198673096ab10a32e544b4500288d78df300b3a1
YongKJ <dxj1718874198@gmail.com>
2021-12-18 18:15:13 +0800

    [update]控制台打印工具类

--- src/common/util/LogUtil.js
+++ src/common/util/LogUtil.js
@@ -1,8 +1,19 @@
 
 export class LogUtil {
+
+    static delimiter() {
+        console.log("========================================================================");
+    }
   
   static info(method, variable, value) {
     console.log(method + " -> " + variable + ": " + value + " ============");
+  }
+
+  static infoInDetail(method, variable, value) {
+      console.log(method + " -> " + variable + ": info");
+      console.log("-----------------------------------");
+      console.log(value);
+      console.log("-----------------------------------");
   }
   
   static error(service, method, e) {
