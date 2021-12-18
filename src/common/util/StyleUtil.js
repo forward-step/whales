aaba5a846167e128ac572568366676e2a5aa16a5
YongKJ <dxj1718874198@gmail.com>
2021-12-18 18:16:20 +0800

    [update]样式工具类

--- src/common/util/StyleUtil.js
+++ src/common/util/StyleUtil.js
@@ -22,22 +22,31 @@ export class StyleUtil {
   }
   
   static getArticleImgStye(imgSrc) {
-    let img = new Image();
-    img.src = imgSrc;
-    
-    LogUtil.info("getArticleImgStye", "img.width", img.width);
-    LogUtil.info("getArticleImgStye", "img.height", img.height);
-    
-    let height = img.height > img.width ? img.height * 0.2 : img.width * 0.2;
-    if (height < 170) {
-      height = 170;
-    }
-    
-    LogUtil.info("getArticleImgStye", "height", height);
-    
-    return {
-      height: height + "px"
-    }
+        let img = new Image();
+        img.src = imgSrc;
+
+        LogUtil.info("getArticleImgStye", "img.width", img.width);
+        LogUtil.info("getArticleImgStye", "img.height", img.height);
+
+        let smallNum = img.width < img.height ? img.width : img.height;
+        let bigNum = img.width > img.height ? img.width : img.height;
+
+        let height = smallNum * 0.2;
+        if (smallNum * 2 < bigNum) {
+            height = bigNum * 0.2;
+        }
+        if (height > 400 || smallNum === 1080) {
+            height = height / 2;
+        }
+        if (height < 170) {
+          height = 170;
+        }
+
+        LogUtil.info("getArticleImgStye", "height", height);
+
+        return {
+          height: height + "px"
+        }
   }
   
 }
